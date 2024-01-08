683cf73658a6a9f2012cc41cfbbd4d8a9983a229
marygang <gangmary2@gmail.com>
2024-01-08 16:58:48 +0900

    test

--- src/main/kotlin/org/example/coha/domain/post/controller/PostController.kt
+++ src/main/kotlin/org/example/coha/domain/post/controller/PostController.kt
@@ -1,8 +1,12 @@
 package org.example.coha.domain.post.controller
 
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
 
+@RequestMapping("/posts")
 @Controller
 class PostController {
-    아무거나
-}
+
+
+
+    }
