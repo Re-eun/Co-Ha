745669600195c4d8c05ee36bbff731bea12e07fc
re-eun <pajeni@naver.com>
2024-01-08 17:15:59 +0900

    깃 테스트

--- src/main/kotlin/org/example/coha/domain/post/controller/PostController.kt
+++ src/main/kotlin/org/example/coha/domain/post/controller/PostController.kt
@@ -9,13 +9,8 @@ import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import org.example.coha.domain.post.service.PostService
 import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
 
 @RequestMapping("/posts")
 @RestController
@@ -44,8 +39,8 @@ class PostController{
         val post: PostResponse = PostService.updatePost(request)
 
         return ResponseEntity
-            .status(HttpStatuis.OK)
-            .body(todoCard)
+            .status(HttpStatus.OK)
+            .body(post)
 
     }
     @GetMapping("/{postId}")
