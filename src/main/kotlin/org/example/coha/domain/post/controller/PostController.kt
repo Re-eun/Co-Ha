31b934c7fa85cfb572b58d9c649684a2fbcf6d80
re-eun <pajeni@naver.com>
2024-01-08 16:59:33 +0900

    세팅

--- src/main/kotlin/org/example/coha/domain/post/controller/PostController.kt
+++ src/main/kotlin/org/example/coha/domain/post/controller/PostController.kt
@@ -1,10 +1,8 @@
 package org.example.coha.domain.post.controller
 
-import org.example.coha.domain.post.dto.PostResponse
 import org.example.coha.domain.post.service.PostService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
