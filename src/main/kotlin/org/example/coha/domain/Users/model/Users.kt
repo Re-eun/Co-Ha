09fdcd38bea92205725d4adaa05808b82e63b0b3
re-eun <pajeni@naver.com>
2024-01-10 19:07:14 +0900

    스프링 시큐리티

--- src/main/kotlin/org/example/coha/domain/Users/model/Users.kt
+++ src/main/kotlin/org/example/coha/domain/Users/model/Users.kt
@@ -1,4 +1,20 @@
 package org.example.coha.domain.Users.model
 
-class Users {
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "users")
+class Users(
+
+    @Column(name = "name")
+    var name: String,
+
+    @Column(name = "password")
+    var userPassword: String,
+
+    @Column(name = "email")
+    var email: String
+) {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null
 }
