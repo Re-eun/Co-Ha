6dbb4a20ccb948d0eed93d110d8acfc18fe7ff9a
re-eun <pajeni@naver.com>
2024-01-13 18:53:18 +0900

    수정 - securtiy deprecated

--- src/main/kotlin/org/example/coha/domain/security/TokenProvider.kt
+++ src/main/kotlin/org/example/coha/domain/security/TokenProvider.kt
@@ -1,9 +1,12 @@
 package org.example.coha.domain.security
 
 import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.io.Decoders
+import io.jsonwebtoken.security.Keys
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.PropertySource
 import org.springframework.stereotype.Service
+import java.security.Key
 import java.sql.Timestamp
 import java.time.Instant
 import java.time.LocalDateTime
@@ -20,9 +23,12 @@ class TokenProvider(
     @Value("\${issuer}")
     private val issuer: String
 ) {
+    val keyBytes: ByteArray = Decoders.BASE64.decode(secretKey)
+    val key: Key = Keys.hmacShaKeyFor(keyBytes)
+
     // user 고유 정보로 토큰 생성
     fun createToken(userSpecification: String) = Jwts.builder()
-        .signWith(io.jsonwebtoken.SignatureAlgorithm.HS256, Base64.getEncoder().encodeToString(secretKey.toByteArray()))
+        .signWith(key, io.jsonwebtoken.SignatureAlgorithm.HS256)
         .setSubject(userSpecification) // 고유 정보로 주체 설정
         .setIssuer(issuer) // 발급자
         .setIssuedAt(Timestamp.valueOf(LocalDateTime.now())) // 발급 시간
@@ -32,7 +38,7 @@ class TokenProvider(
 
     // 토큰의 subject 를 복호화하여 문자열 형태로 반환 ( 유효한지 확인, 유효하지 않을 경우 null )
     fun validateTokenAndGetSubject(token: String): String? = Jwts.parserBuilder()
-        .setSigningKey(secretKey.toByteArray()) // 비밀키로 복호화
+        .setSigningKey(key) // 비밀키로 복호화
         .build()
         .parseClaimsJws(token) // 파싱 : jwt 형식에 맞게 데이터를 해석하고 추출
         .body // 토큰의 본문 (claims) 에 접근
